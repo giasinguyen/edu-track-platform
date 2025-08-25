510ed163c06f7ade2055ce6c9330d59ea48b04a3
giasinguyen <giasinguyentran@gmail.com>
2025-08-25 08:55:41 +0700

    EduTrack: First Commit!

---
+++ EduTrack-BackEnd/src/main/java/com/edutrack/edutrackbackend/EduTrackBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.edutrack.edutrackbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduTrackBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduTrackBackEndApplication.class, args);
+    }
+
+}
---
+++ EduTrack-BackEnd/src/test/java/com/edutrack/edutrackbackend/EduTrackBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edutrack.edutrackbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduTrackBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
